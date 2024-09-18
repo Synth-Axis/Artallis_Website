f79a8c2ca48fda75cbbb67f4bad6db2f7c6397ad
Synth-Axis <synth-etiks@hotmail.com>
2024-09-18 17:34:12 +0100

    setup

---
+++ Artallis_Website/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Aqui vai nascer um website espetacular</h1>
+      <h2>By: Synth-Axis @CreativeMinds</h2>
+    </>
+  );
+}
+
+export default App;
