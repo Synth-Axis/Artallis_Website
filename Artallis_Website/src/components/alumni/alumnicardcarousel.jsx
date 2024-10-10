7949f13656dbd1393baaa2d5b253d618c06537b3
Synth-Axis <synth-etiks@hotmail.com>
2024-10-10 10:34:36 +0100

    mobile update2

--- Artallis_Website/src/components/alumni/alumnicardcarousel.jsx
+++ Artallis_Website/src/components/alumni/alumnicardcarousel.jsx
@@ -23,43 +23,41 @@ const Alumnicardcarousel = ({ persons }) => {
   const { id, foto, nome, instrumento } = persons[currentPersonIndex];
 
   return (
-    <div className="person-carousel-container">
-      <div className="person-carousel">
-        <div className="person-card">
-          <div className="person-card__image">
-            <img src={foto} alt={nome} />
-          </div>
-          <div className="person-card__details">
-            <h1>{nome}</h1>
-            <h5>{instrumento}</h5>
-          </div>
-          <div className="person-card__nav">
-            <button className="prev" onClick={handlePrev}>
-              &#9664;
-            </button>
-            <button className="next" onClick={handleNext}>
-              &#9654;
-            </button>
-          </div>
+    <div className="person-carousel">
+      <div className="person-card">
+        <div className="person-card__image">
+          <img src={foto} alt={nome} />
         </div>
+        <div className="person-card__details">
+          <h1>{nome}</h1>
+          <h5>{instrumento}</h5>
+        </div>
+        <div className="person-card__nav">
+          <button className="prev" onClick={handlePrev}>
+            &#9664;
+          </button>
+          <button className="next" onClick={handleNext}>
+            &#9654;
+          </button>
+        </div>
+      </div>
 
-        <div className="document-section">
-          <div className="document-tile">
-            <p>MATRIZES</p>
-            <p className="plus">&#43;</p>
-          </div>
-          <div className="document-tile">
-            <p>PROGRAMAS</p>
-            <p className="plus">&#43;</p>
-          </div>
-          <div className="document-tile">
-            <p>DOCUMENTOS OFICIAIS</p>
-            <p className="plus">&#43;</p>
-          </div>
-          <div className="document-tile">
-            <p>DOCUMENTOS AULAS</p>
-            <p className="plus">&#43;</p>
-          </div>
+      <div className="document-section">
+        <div className="document-tile">
+          <p>MATRIZES</p>
+          <p className="plus">&#43;</p>
+        </div>
+        <div className="document-tile">
+          <p>PROGRAMAS</p>
+          <p className="plus">&#43;</p>
+        </div>
+        <div className="document-tile">
+          <p>DOCUMENTOS OFICIAIS</p>
+          <p className="plus">&#43;</p>
+        </div>
+        <div className="document-tile">
+          <p>DOCUMENTOS AULAS</p>
+          <p className="plus">&#43;</p>
         </div>
       </div>
     </div>
